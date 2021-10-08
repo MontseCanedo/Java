63e29aa7c20c9e28ee3a79c183444dbcfc44f37b
Montserrat Canedo <vmontse_cgutierrez@hotmail.com>
2021-10-07 22:58:05 -0400

    Completando modificadores de acceso y sobreescritura

--- Programacion/ModificadoresAcceso/src/paquete1/Clase1.java
+++ Programacion/ModificadoresAcceso/src/paquete1/Clase1.java
@@ -2,11 +2,22 @@ package paquete1;
 
 public class Clase1 {
     
-    public String atributoPublico = "Valor atributo publico";
+    //public String atributoPublico = "Valor atributo publico";
+    protected String atributoProtected = "Valor atributo protected";
 
-    public Clase1()
+    /*public Clase1()
     {
         System.out.println("Constructor publico");
+    }*/
+    
+    public Clase1(String arg)
+    {
+        System.out.println("Constructor publico");
+    }
+    
+    protected Clase1()
+    {
+        System.out.println("Constructor protected");
     }
     
     public void metodoPublico()
@@ -14,5 +25,8 @@ public class Clase1 {
         System.out.println("Metodo publico");
     }
     
-    
+    protected void metodoProtected()
+    {
+        System.out.println("Metodo protected");
+    }
 }
--- Programacion/ModificadoresAcceso/src/test/TestModificadoresAcceso.java
+++ Programacion/ModificadoresAcceso/src/test/TestModificadoresAcceso.java
@@ -1,12 +1,16 @@
 package test;
 
 import paquete1.Clase1;
+import paquete2.ClaseHija;
 
 public class TestModificadoresAcceso {
     
     public static void main(String[] args) {
-        Clase1 clase1 = new Clase1();
-        System.out.println("clase1 = " + clase1.atributoPublico);
-        clase1.metodoPublico();
+        //Clase1 clase1 = new Clase1("Publico");
+        //System.out.println("clase1 = " + clase1.atributoPublico);
+        //clase1.metodoPublico();
+        
+        ClaseHija claseHija = new ClaseHija();
+        System.out.println("clasehija = " + claseHija);
     }
 }
