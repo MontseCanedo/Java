429c640c27d9b87ac296d3d017bfc7448bf31fd4
MontseCanedo <montseykg@gmail.com>
2024-09-09 16:58:32 -0600

    menu aplicacion zonafit

--- ZonaFit/src/main/java/ZonaFit.java
+++ ZonaFit/src/main/java/ZonaFit.java
@@ -1,14 +1,7 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- */
 
-/**
- *
- * @author ADMIN
- */
 public class ZonaFit {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        
     }
 }
---
+++ ZonaFit/src/main/java/zona_fit/presentacion/ZonaFitApp.java
@@ -0,0 +1,109 @@
+package zona_fit.presentacion;
+
+import java.util.Scanner;
+import zona_fit.datos.ClienteDAO;
+import zona_fit.datos.IClienteDAO;
+import zona_fit.dominio.Cliente;
+
+public class ZonaFitApp {
+
+    public static void main(String[] args) {
+        zonaFitApp();
+    }
+
+    private static void zonaFitApp() {
+        var salir = false;
+        Scanner sc = new Scanner(System.in);
+        IClienteDAO clienteDao = new ClienteDAO();
+        while (!salir) {
+            try {
+                var opcion = mostrarMenu(sc);
+                salir = ejecutarOpciones(sc, opcion, clienteDao);
+            } catch (Exception e) {
+                System.out.println("Error al ejecutar opciones" + e.getMessage());
+            }
+            System.out.println();
+        }
+    }
+
+    private static int mostrarMenu(Scanner sc) {
+
+        System.out.print("""
+                           Seleccione una opcion:
+                           1. Listar clientes
+                           2. Buscar cliente
+                           3. Agregar cliente
+                           4. Modificar cliente
+                           5. Eliminar cliente
+                           6. Salir\s""");
+
+        return Integer.parseInt(sc.nextLine());
+    }
+
+    private static boolean ejecutarOpciones(Scanner sc, int opcion, IClienteDAO clienteDAO) {
+        var salir = false;
+        //IClienteDAO clienteDao = new ClienteDAO();
+        switch (opcion) {
+            case 1 -> {
+                System.out.println("--- Listado de clientes ---");
+                var clientes = clienteDAO.listarClientes();
+                clientes.forEach(System.out::println);
+            }
+            case 2 -> {
+                System.out.println("Ingrese el id del cliente a buscar:");
+                var id = sc.nextInt();
+                var cliente = new Cliente(id);
+                var busqueda = clienteDAO.buscarClientePorId(cliente);
+                if (busqueda) {
+                    System.out.println("Cliente encontrado = " + cliente);
+                } else {
+                    System.out.println("No se encontro cliente: " + cliente.getId());
+                }
+            }
+            case 3 -> {
+                System.out.println("Ingrese los datos del cliente a agregar:");
+                var nombre = sc.nextLine();
+                var apellido = sc.nextLine();
+                var membresia = sc.nextInt();
+                var cliente = new Cliente(nombre, apellido, membresia);
+                var clientes = clienteDAO.agregarCliente(cliente);
+                System.out.println("cliente agregado: " + cliente);
+                if (clientes) {
+                    System.out.println("Cliente agregado: " + cliente);
+                } else {
+                    System.out.println("No se pudo agregar cliente: " + cliente);
+                }
+            }
+            case 4 -> {
+                System.out.println("Ingrese el id del cliente a modificar:");
+                var id = sc.nextInt();
+                System.out.print("Nombre ");
+                var nombre = sc.nextLine();
+                System.out.println();
+                System.out.print("Apellido ");
+                var apellido = sc.nextLine();
+                System.out.println();
+                System.out.print("Membresia ");
+                var membresia = sc.nextInt();
+                var cliente = new Cliente(id, nombre, apellido, membresia);
+                var clientes = clienteDAO.modificarCliente(cliente);
+                System.out.println("cliente modificado: " + cliente);
+                if (clientes) {
+                    System.out.println("Cliente agregado: " + cliente);
+                } else {
+                    System.out.println("No se pudo agregar cliente: " + cliente);
+                }
+            }
+//            case 1 -> {
+//                System.out.println("--- Listado de clientes ---");
+//                var clientes = clienteDAO.listarClientes();
+//                clientes.forEach(System.out::println);
+//            }
+            case 6 -> {
+                System.out.println("Saliendo...");
+                salir = true;
+            }
+        }
+        return salir;
+    }
+}
